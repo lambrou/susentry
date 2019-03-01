52afafda74dfe33b32fd37c8ead385fddcd37da8
Alexander <alexanderlambrou0602@gmail.com>
2019-03-01 02:58:40 -0700

    Update makeroot.sh

--- makeroot.sh
+++ makeroot.sh
@@ -2,9 +2,9 @@
 
 # To call this script: sudo ./makeroot.sh /path/to/susentry/dir/
 
-# First, make the owner of the entire susentry directory root.
-# the -R flag makes it so it will recursively set every file and folder in this
-# directory as root.
+# Makes the owner of the file/directory root.
+# the -R flag makes it so it will recursively set every 
+# file and folder in a directory as root.
 sudo chown -R root:root $1
 
 # Then, give root executable permissions
